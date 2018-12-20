a9bbff5d7b870d3106d99597eac65b6bf2c31fef
semiuniversal <w.tremblay@gmail.com>
2018-12-19 21:32:14 -0500

    Ready for Conditional rendering @55m

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
